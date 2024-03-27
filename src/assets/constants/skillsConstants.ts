ecf1e3cc16701653cf7215d1cdcd068f765198bd
Udit Sharma <udit.sharma01@nagarro.com>
2024-03-27 15:23:13 +0530

    added skills

---
+++ src/assets/constants/skillsConstants.ts
@@ -0,0 +1,60 @@
+import { SkillsData } from "@/utils/Skills";
+
+export const SKILL_DATA: SkillsData[] = [
+    {
+        "title": "Backend",
+        "tech": [
+            "Java",
+            "Spring-Boot",
+            "Spring MVC",
+            "Microservices",
+            "Spring Security",
+            "OAuth 2.0",
+            "JUnit",
+            "Node Js",
+            "Spring Data JPA"
+        ]
+    },
+    {
+        "title": "Frontend",
+        "tech": [
+            "HTML",
+            "CSS",
+            "Tailwind",
+            "Javascript",
+            "React Js",
+            "Angular Js",
+            "Axios",
+            "Bootstrap"
+        ]
+    },
+    {
+        "title": "Devops",
+        "tech": [
+            "Docker",
+            "Docker Compose",
+            "GIT",
+            "Jenkins",
+            "Amazon Web Services"
+        ]
+    },
+    {
+        "title": "Database",
+        "tech": [
+            "MySql",
+            "MongoDB",
+            "Redis",
+            "RDS"
+        ]
+    },
+    {
+        "title": "Additional Tools",
+        "tech": [
+            "Kafka",
+            "Postman",
+            "IntelliJ IDEA",
+            "Swagger",
+            "VS-Code"
+        ]
+    }
+];
--- src/pages/skills/Skills.tsx
+++ src/pages/skills/Skills.tsx
@@ -1,6 +1,22 @@
+import { SKILL_DATA } from "@/assets/constants/skillsConstants"
+import TechCard from "./TechCard"
+
 function Skills() {
     return (
-        <div className="text-center"> WIP-Skills</div>
+        <section>
+            <div className="-my-6">
+                <div className="mx-auto px-6 max-w-6xl text-gray-500">
+                    <div className="text-center">
+                        <h2 className="text-6xl text-gray-950 dark:text-white font-semibold font-logo">Skills</h2>
+                    </div>
+                    <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+                        {SKILL_DATA.map((skills, index) => (
+                            <TechCard key={index} title={skills.title} tech={skills.tech} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
